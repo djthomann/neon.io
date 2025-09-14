2087c20cd3226874c6d730a93d9267b7f5b34526
David Thomann <david.joshua.thomann@gmail.com>
2025-09-14 16:25:21 +0200

    Initialized Backend

---
+++ src/test/kotlin/de/tho/neon/neon/io/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.tho.neon.neon.io
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
