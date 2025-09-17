44550630fc25be899a24ae709f601eac70b2749f
David Thomann <david.joshua.thomann@gmail.com>
2025-09-18 00:44:31 +0200

    Added UserComponent and Game Icon

---
+++ frontend/src/components/UserComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="user-info">
+    <p>Welcome {{ userStore.username }}! Your id is: {{ userStore.id }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/user'
+
+const userStore = useUserStore()
+</script>
+
+<style scoped></style>
